e709b4fd035eb8833ab5598d79b3e16b006cb1d3
Jacob Jeffries <jeffrij85@gmail.com>
2023-03-09 16:07:33 -0500

    Connection to DB created & tested, first of 7 functions created

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const inq = require('inquirer');
+// const inq = require('inquirer');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+require('dotenv').config();
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  },
+  console.log(`Connected to Database: ${process.env.DB_NAME}, as ${process.env.DB_USER}.`)
+);
+
+function viewDepartments() {
+  const sql = `SELECT * FROM department`;
+
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.log(err)
+    }else{
+      console.table(result);
+    }
+  })
+ 
+};
+
+viewDepartments();

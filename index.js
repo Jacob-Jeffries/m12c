e7960aa22fcb0665b0b3a09b7db65077fe23a221
Jacob Jeffries <jeffrij85@gmail.com>
2023-03-09 16:24:45 -0500

    Got the async await promise working for mysql

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 // const inq = require('inquirer');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const cTable = require('console.table');
 require('dotenv').config();
 
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-  },
-  console.log(`Connected to Database: ${process.env.DB_NAME}, as ${process.env.DB_USER}.`)
-);
 
-function viewDepartments() {
+async function viewDepartments() {
+  const db = await mysql.createConnection(
+    {
+      host: 'localhost',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    },
+    console.log(`Connected to Database: ${process.env.DB_NAME}, as ${process.env.DB_USER}.`)
+  );
+
   const sql = `SELECT * FROM department`;
 
-  db.query(sql, (err, result) => {
-    if (err) {
-      console.log(err)
-    }else{
-      console.table(result);
-    }
-  })
- 
+  const [rows] = await db.execute(sql);
+
+  console.table(rows); 
+  process.exit(0);
 };
 
 viewDepartments();

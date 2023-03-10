bd55d3304be005ab1ec60620b6dbf4ba1a0ebd00
Jacob Jeffries <jeffrij85@gmail.com>
2023-03-10 18:51:32 -0500

    Created addRole(), along with the associated getDepartments() - used parseInt() this time for ID's

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ async function getNames(){
   const sql = `SELECT CONCAT(id,' ',first_name,' ',last_name) AS name FROM employee`;
   const [rows] = await db.execute(sql);
   let names = ['No Manager'];
-  Object.values(rows).forEach((name) => {names.push(name.name)});
+  Object.values(rows).forEach((row) => {names.push(row.name)});
   return names;
 };
 
@@ -62,37 +62,73 @@ async function getRoles(){
   const sql = `SELECT title FROM roles`;
   const [rows] = await db.execute(sql);
   let roles = [];
-  Object.values(rows).forEach((role) => {roles.push(role.title)});
+  Object.values(rows).forEach((row) => {roles.push(row.title)});
   return roles;
+};
+
+async function getDepartments(){
+  const db = await connect();
+  const sql = `SELECT CONCAT(id,' ',dept_name) AS dept FROM department`;
+  const [rows] = await db.execute(sql);
+  let depts = [];
+  Object.values(rows).forEach((row) => {depts.push(row.dept)});
+  return depts;
 };
 
 async function addDepartment(){
  const db = await connect();
-
+ display();
  const query = [
-  {
-    name: 'dept',
-    type: 'text',
-    message: 'Please enter the name of the new Department'
-  }
-];
+    {
+      name: 'dept',
+      type: 'text',
+      message: 'Please enter the name of the new Department'
+    }
+  ];
+  const department = await inquirer.prompt(query);
+  const {dept} = department;
+  const sql = `INSERT INTO department (dept_name) VALUES (?)`;
+  const [rows] = await db.execute(sql, [dept])
+  viewDepartments();
+};
 
-const department = await inquirer.prompt(query);
-const {dept} = department;
-
-const sql = `INSERT INTO department (dept_name) VALUES (?)`;
-const [rows] = await db.execute(sql, [dept])
-
-viewDepartments();
+async function addRole(){
+  const db = await connect();
+  const depts = await getDepartments();
+  display();
+  const query = [
+    {
+      name: 'title',
+      type: 'text',
+      message: 'Please enter the Title of the new Role: '
+    },
+    {
+      name: 'salary',
+      type: 'number',
+      message: 'Please enter the Salary for this new Role (whole dollars): $',
+    },
+    {
+      name: 'department_id',
+      type: 'list',
+      message: 'Please select the associated Department for this new Role:',
+      choices: depts,
+      loop: false
+    }
+  ];
+  const department = await inquirer.prompt(query);
+  const {title, salary, department_id} = department;
+  const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`;
+  // LOL parseINT() is so much easier than what I did earlier to get the id!
+  // I am going to leave both examples of ways to do it since I did both.
+  const id = parseInt(department_id);
+  const [rows] = await db.execute(sql, [title, salary, id])
+  viewRoles();
 };
 
 async function addEmployee() {
   const db = await connect();
-
   const names = await getNames();
   const roles = await getRoles();
-
-
   display();
   console.log('\n');
   const query = [
@@ -110,31 +146,25 @@ async function addEmployee() {
       name: 'rid',
       type: 'list',
       message: 'Please select the new employee\'s Title: ',
-      choices: roles
+      choices: roles,
+      loop: false
     },
     {
       name: 'man',
       type: 'list',
       message: 'Please select the new employee\'s Manager: ',
-      choices: names
+      choices: names,
+      loop: false
     }
-  ]
-
+  ];
   const nEmp = await inquirer.prompt(query);
   const role_id = roles.indexOf(nEmp.rid) + 1;
-
   const { fn, ln, man } = nEmp;
   let manager = names.indexOf(nEmp.man);
-  
-  if(man === 'No Manager'){
-    manager = null;
-  }
-
+  if(man === 'No Manager'){manager = null};
   const sql = 'INSERT INTO employee (first_name, last_name, roles_id, manager) VALUES(?,?,?,?)';
   const [rows] = await db.execute(sql, [fn, ln, role_id, manager]);
-
   viewEmployees();
-
 };
 
 function mainMenu(){
@@ -149,15 +179,15 @@ function mainMenu(){
         "Add Employee",
         "NO - Update Employee Role",
         "View All Roles",
-        "NO - Add Role",
+        "Add New Role",
         "View All Departments",
         "Add New Department",
         "Clear Screen",
         "Quit"
-      ]
-    },
-  ]
-
+      ],
+      loop: false
+    }
+  ];
   return inquirer.prompt(query);
 };
 
@@ -171,13 +201,14 @@ function display(){
 
 async function main(){
   const { mMenu } = await mainMenu();
-
   if(mMenu ===  "View All Employees"){
     viewEmployees();
   }else if(mMenu === 'Add Employee'){
     addEmployee();
   }else if(mMenu === 'View All Roles'){
     viewRoles();
+  }else if(mMenu === 'Add New Role'){
+    addRole();
   }else if(mMenu === 'View All Departments'){
     viewDepartments();
   }else if(mMenu === 'Add New Department'){

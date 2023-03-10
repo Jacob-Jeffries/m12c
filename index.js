ad6292fd911a08ce4944c029c76c200ef28690bc
Jacob Jeffries <jeffrij85@gmail.com>
2023-03-10 11:11:35 -0500

    Fucked around and found out about inquier 9.x.x! Oh boy that was a pain in the ass - sticking to 8.2.4 for now!

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// const inq = require('inquirer');
 const mysql = require('mysql2/promise');
 const cTable = require('console.table');
+const inquirer = require('inquirer');
 require('dotenv').config();
 
 async function connect() {  
@@ -16,28 +16,61 @@ const db = await mysql.createConnection(
   return db;
 };
 
-
 async function viewDepartments() {
-
   const db = await connect();
-
   const sql = `SELECT * FROM department`;
-
   const [rows] = await db.execute(sql);
-
   console.table(rows); 
 };
 
 async function viewRoles() {
-
   const db = await connect();
-
   const sql = `SELECT * FROM roles`;
-
   const [rows] = await db.execute(sql);
-
   console.table(rows); 
 };
 
-viewDepartments();
-viewRoles();
+async function viewEmployees() {
+  const db = await connect();
+  const sql = `SELECT * FROM employee`;
+  const [rows] = await db.execute(sql);
+  console.table(rows); 
+};
+
+function mainMenu(){
+  const query = [
+    {
+      name: "mMenu",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "NO - Add Employee",
+        "NO - Update Employee Role",
+        "View All Roles",
+        "NO - Add Role",
+        "View All Departments",
+        "Add Department",
+        "Quit"
+      ]
+    },
+  ]
+
+  return inquirer.prompt(query);
+};
+
+async function main(){
+  console.log(`\n                ----------                \n`);
+  console.log(`Welcome to Jacob's Employee Managment System\n`);
+  console.log(`         Copyright 2023 Jacob Jeffries      `);
+  console.log(`\n                ----------                \n`);
+
+  const { mMenu } = await mainMenu();
+
+  if(mMenu === "View All Employees"){
+    viewEmployees();
+  }
+
+};
+
+main();
